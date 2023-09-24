321b8358431cbdbc508fc2b6c4f14e9c816262de
Florent Teissier <teissierflorent@gmail.com>
2023-09-24 10:55:24 +0200

    ignore prettier in utils test for better readability

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -4,37 +4,11 @@ import { mkHand } from "../parse";
 
 describe("utils", () => {
   test("should sort ranks properly", () => {
-    expect(
-      sortRanks([
-        "2",
-        "8",
-        "J",
-        "Q",
-        "T",
-        "K",
-        "A",
-        "3",
-        "9",
-        "4",
-        "5",
-        "6",
-        "7",
-      ])
-    ).toEqual([
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "T",
-      "J",
-      "Q",
-      "K",
-      "A",
-    ]);
+    // prettier-ignore
+    const expected = sortRanks(["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]);
+    // prettier-ignore
+    const result = sortRanks(["2", "8", "J", "Q", "T", "K", "A", "3", "9", "4", "5", "6", "7"])
+    expect(result).toEqual(expected);
   });
   test("sortHand should put higher kicker first", () => {
     expect(sortHand(mkHand("2hAh"))).toEqual(mkHand("Ah2h"));
